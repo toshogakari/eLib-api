b1cec8156ba566d374b4e7b5ab9d4b928ee998d5
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 09:40:46 +0000

    add index

--- db/migrate/20161108015326_create_tag_has_books.rb
+++ db/migrate/20161108015326_create_tag_has_books.rb
@@ -1,8 +1,8 @@
 class CreateTagHasBooks < ActiveRecord::Migration[5.0]
   def change
     create_table :tag_has_books do |t|
-      t.references :book, foreign_key: true
-      t.references :tag, foreign_key: true
+      t.references :book, foreign_key: true, index: true
+      t.references :tag, foreign_key: true, index: true
 
       t.timestamps
     end
