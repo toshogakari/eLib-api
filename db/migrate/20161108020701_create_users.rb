fedaf9d9431c3afd31463f9a067b162fb7fb69fe
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 09:42:05 +0000

    add limit

--- db/migrate/20161108020701_create_users.rb
+++ db/migrate/20161108020701_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
-      t.integer :experience
+      t.string :name, null: false, default: ''
+      t.string :email, null: false, default: ''
+      t.string :password_digest, null: false, default: ''
+      t.integer :experience, limit: 1, null: false, default: 0
       t.text :profile
+      t.references :job, index: true, foreign_key: true
 
       t.timestamps
     end
