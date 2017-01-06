278e1acc82a2b180b4f41cb14421569ca59a2eec
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 10:03:54 +0000

    rename migration file

--- db/migrate/20161108015326_create_tag_has_books.rb
+++ db/migrate/20161108015326_create_tag_has_books.rb
@@ -1,8 +1,8 @@
 class CreateTagHasBooks < ActiveRecord::Migration[5.0]
   def change
     create_table :tag_has_books do |t|
-      t.references :book, foreign_key: true, index: true
-      t.references :tag, foreign_key: true, index: true
+      t.references :book, index: true, foreign_key: true
+      t.references :tag, index: true, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20161108015567_create_user_has_skills.rb
+++ db/migrate/20161108015567_create_user_has_skills.rb
@@ -1,8 +1,8 @@
 class CreateUserHasSkills < ActiveRecord::Migration[5.0]
   def change
     create_table :user_has_skills do |t|
-      t.references :skill, foreign_key: true, index: true
-      t.references :user, foreign_key: true, index: true
+      t.references :skill, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps
     end
