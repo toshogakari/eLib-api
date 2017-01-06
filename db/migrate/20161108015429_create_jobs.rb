1ad18a6085d106339b50c8fc7123cd113bc52c67
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 09:41:14 +0000

    add limit

--- db/migrate/20161108015429_create_jobs.rb
+++ db/migrate/20161108015429_create_jobs.rb
@@ -1,7 +1,7 @@
 class CreateJobs < ActiveRecord::Migration[5.0]
   def change
     create_table :jobs do |t|
-      t.string :name
+      t.string :name, limit: 255, null: false, default: ''
 
       t.timestamps
     end
