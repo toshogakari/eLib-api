bee6d0b81e87520f4a9aa49fb756d8a55fcedf61
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 09:04:15 +0000

    add book relation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  belongs_to :book_cases
+  has_many :book_cases
+  has_many :users, through: :book_cases
+  has_many :tag_has_books
+  has_many :tag, through: :tag_has_books
 end
