f03c226bd9378a7304449907640b0727987b9b2c
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 09:41:24 +0000

    add limit

--- db/migrate/20161108015517_create_skills.rb
+++ db/migrate/20161108015517_create_skills.rb
@@ -1,7 +1,7 @@
 class CreateSkills < ActiveRecord::Migration[5.0]
   def change
     create_table :skills do |t|
-      t.string :name
+      t.string :name, limit: 45, null: false, default: ''
 
       t.timestamps
     end
