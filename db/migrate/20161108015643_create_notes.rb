0cf5f13a26e3e95c54ba5c8b2ae9c3c26c73d1bb
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 09:41:37 +0000

    add reference

--- db/migrate/20161108015643_create_notes.rb
+++ db/migrate/20161108015643_create_notes.rb
@@ -1,7 +1,8 @@
 class CreateNotes < ActiveRecord::Migration[5.0]
   def change
     create_table :notes do |t|
-      t.text :note
+      t.text :note, null: false, default: ''
+      t.references :book_case, index: true, foreign_key: true
 
       t.timestamps
     end
