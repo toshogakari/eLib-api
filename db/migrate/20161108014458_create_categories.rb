f76337fdf1fd156e96af701343d9c0d9b794fdc2
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 09:40:17 +0000

    add limit

--- db/migrate/20161108014458_create_categories.rb
+++ db/migrate/20161108014458_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
-      t.string :name
+      t.string :name, limit: 45, null: false, default: ''
 
       t.timestamps
     end
