a6e0538eae573c06e35576f64f25cc060d640987
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 10:04:11 +0000

    fix word

--- db/migrate/20161108015603_create_book_cases.rb
+++ db/migrate/20161108015603_create_book_cases.rb
@@ -3,8 +3,8 @@ class CreateBookCases < ActiveRecord::Migration[5.0]
     create_table :book_cases do |t|
       t.integer :evaluation, limit: 1, null: false, default: 0
       t.integer :level, null: false, default: 0
-      t.references: :user, index: true, foreign_key: true
-      t.references: :book, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.references :book, index: true, foreign_key: true
 
       t.timestamps
     end
