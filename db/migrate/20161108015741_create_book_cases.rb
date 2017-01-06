334ba4e395aefbe5b2c3991f29f7b2c6489b8233
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 09:41:51 +0000

    add reference

--- db/migrate/20161108015741_create_book_cases.rb
+++ db/migrate/20161108015741_create_book_cases.rb
@@ -1,8 +1,10 @@
 class CreateBookCases < ActiveRecord::Migration[5.0]
   def change
     create_table :book_cases do |t|
-      t.integer :evaluation
-      t.integer :level
+      t.integer :evaluation, limit: 1, null: false, default: 0
+      t.integer :level, null: false, default: 0
+      t.references: :user, index: true, foreign_key: true
+      t.references: :user, index: true, foreign_key: true
 
       t.timestamps
     end
