cc7e18a4def9fe3ab9ca7a1a726bf119f8295822
Tsukasa Arima <pyar6329@gmail.com>
2017-01-06 09:40:01 +0000

    add limit

--- db/migrate/20161108014210_create_tags.rb
+++ db/migrate/20161108014210_create_tags.rb
@@ -1,7 +1,7 @@
 class CreateTags < ActiveRecord::Migration[5.0]
   def change
     create_table :tags do |t|
-      t.string :name
+      t.string :name, limit: 45, null: false, default: ''
 
       t.timestamps
     end
