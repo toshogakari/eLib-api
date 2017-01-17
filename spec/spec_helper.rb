f04b8bf31c5aac8750a9630fac045b1f618f9809
Tsukasa Arima <pyar6329@gmail.com>
2017-01-17 08:28:05 +0000

    fix bullet configfile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,14 +96,4 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-  if Bullet.enable?
-    config.before(:each) do
-      Bullet.start_request
-    end
-
-    config.after(:each) do
-      Bullet.perform_out_of_channel_notifications if Bullet.notification?
-      Bullet.end_request
-    end
-  end
 end
