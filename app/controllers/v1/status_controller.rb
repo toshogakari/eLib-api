95dba4a5c46c05e95f77cf2674ba1018e7720a93
Tsukasa Arima <pyar6329@gmail.com>
2017-01-20 10:58:23 +0000

    add /v1/status

--- app/controllers/v1/status_controller.rb
+++ app/controllers/v1/status_controller.rb
@@ -1,2 +1,11 @@
 class V1::StatusController < ApplicationController
+  # before_action :authenticate_user
+  def index
+    status = {
+      errors:  "500001",
+      message: "Server error, ErrorException"
+    }
+    render json: status
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
+  root 'v1/status#index'
+  namespace :v1 do
+    resources :status, only: [:index]
+  end
+  # post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
